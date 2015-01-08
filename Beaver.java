3bb0170dba153f370f6cd66363d43e9e8ba55bc4
Tranteon <agklein98@gmail.com>
2015-01-07 21:05:10 -0700

    began working, attempting to get beavers going in set directions

--- Beaver.java
+++ Beaver.java
@@ -4,17 +4,18 @@ import battlecode.common.*;
 
 public class Beaver extends BaseRobot {
 	public RobotController rc;
-	
+	public int timesMoved;
 	public Beaver(RobotController rcin){
 		super(rcin);
 		rc = rcin;
+		timesMoved = 0;
 	}
 	
 	@Override
 	
 	public void run() throws GameActionException {
 		Direction dir = getRandomDirection();
-		if(rc.isCoreReady()&&rc.canMove(dir)){
+		if(rc.isCoreReady()&&rc.canMove(dir)&&timesMoved<6){
 			rc.move(dir);
 			
 		}
