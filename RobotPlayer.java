e9c7a5425e1019c2becf1b1e7767fb13eb7adb51
robbotorigami <robbotorigami@gmail.com>
2015-01-15 03:25:38 -0700

    Basic drone explorer

--- RobotPlayer.java
+++ RobotPlayer.java
@@ -10,8 +10,12 @@ public class RobotPlayer {
 	public static Direction toBoldlyGo;
 	public static Random randall;
 	public static int missionCounter;
+	public static int widthOfMap = 0;
+	public static int heightOfMap = 0;
+	public static MapLocation centerOfMap;
 	public static void run(RobotController rcin){
 		rc = rcin;
+		centerOfMap = new MapLocation((rc.senseHQLocation().x + rc.senseEnemyHQLocation().x)/2, (rc.senseHQLocation().y + rc.senseEnemyHQLocation().y)/2);
 		randall = new Random(rc.getID());
 		missionCounter = 0;
 		if(robotsOfTypeOnTeam(RobotType.MINERFACTORY, rc.getTeam())<4){
@@ -27,6 +31,11 @@ public class RobotPlayer {
 					shootWeakest();
 					if(rc.getTeamOre()>510)
 						spawnUnit(RobotType.BEAVER);
+					if(Clock.getRoundNum()%500 ==0 && rc.readBroadcast(10) !=0){
+						widthOfMap = Math.abs(centerOfMap.x - rc.readBroadcast(10))*2 + 4;
+						heightOfMap = Math.abs(centerOfMap.y - rc.readBroadcast(11))*2 + 4;
+						printMap();
+					}
 					break;
 				case BEAVER:
 					shootWeakest();
@@ -71,15 +80,93 @@ public class RobotPlayer {
 				case TANK:
 					shootWeakest();
 					moveAsCloseToDirection(rc.getLocation().directionTo(rc.senseEnemyTowerLocations()[0]));
+					break;
 				case HELIPAD:
 					spawnUnit(RobotType.DRONE);
+					break;
 				case DRONE:
+					
+					if(rc.readBroadcast(10) == 0){
+						MapLocation toTheLeft = rc.getLocation().add(rc.senseEnemyHQLocation().directionTo(rc.senseHQLocation()).rotateLeft());
+						MapLocation toTheRight = rc.getLocation().add(rc.senseEnemyHQLocation().directionTo(rc.senseHQLocation()).rotateRight());
+						boolean left = rc.senseHQLocation().x < rc.senseEnemyHQLocation().x;
+						boolean down = rc.senseHQLocation().y > rc.senseEnemyHQLocation().y;
+						Direction dir;
+						if(!left){
+							if(down){
+								dir = Direction.SOUTH_EAST;
+							}else{
+								dir = Direction.NORTH_EAST;
+							}
+						}else{
+							if(down){
+								dir = Direction.SOUTH_WEST;
+							}else{
+								dir = Direction.NORTH_WEST;
+							}
+						}
+						MapLocation target = centerOfMap.add(dir, (int) (Math.sqrt(rc.senseEnemyHQLocation().distanceSquaredTo(rc.senseHQLocation()))/(2*1.4)));
+						System.out.println(centerOfMap + " + " + dir + " = " +target );
+						while(rc.senseTerrainTile(toTheLeft) != TerrainTile.OFF_MAP || rc.senseTerrainTile(toTheRight) != TerrainTile.OFF_MAP){
+							target = target.add(dir);
+							System.out.println(target);
+							pathToLocation(target, 0);
+							toTheLeft = rc.getLocation().add(rc.senseEnemyHQLocation().directionTo(rc.senseHQLocation()).rotateLeft());
+							toTheRight = rc.getLocation().add(rc.senseEnemyHQLocation().directionTo(rc.senseHQLocation()).rotateRight());
+						}
+						System.out.println(rc.getLocation() + "LO");
+						rc.broadcast(10, rc.getLocation().x);
+						rc.broadcast(11, rc.getLocation().y);
+					}
+					widthOfMap = Math.abs(centerOfMap.x - rc.readBroadcast(10))*2 + 4;
+					heightOfMap = Math.abs(centerOfMap.y - rc.readBroadcast(11))*2 + 4;
 					scanPath();
+					break;
 				}
 			}catch(GameActionException e){
 				e.printStackTrace();
 			}
 			rc.yield();
+		}
+	}
+	private static void printMap() throws GameActionException {
+		System.out.println(Clock.getRoundNum());
+		System.out.println(widthOfMap);
+		System.out.println(heightOfMap);
+		System.out.println((centerOfMap.x - widthOfMap/2) + ", " + (centerOfMap.x + widthOfMap/2));
+		System.out.println((centerOfMap.y - heightOfMap/2) + ", " + (centerOfMap.y + heightOfMap/2));
+		for(int j = centerOfMap.y - heightOfMap/2; j < centerOfMap.y + heightOfMap/2; j++){
+			for(int i = centerOfMap.x - widthOfMap/2; i < centerOfMap.x + widthOfMap/2; i++){
+
+				if(rc.canSenseLocation(new MapLocation(i,j)) && rc.senseRobotAtLocation(new MapLocation(i,j)) != null){
+					RobotType type = rc.senseRobotAtLocation(new MapLocation(i,j)).type;
+					if(type == RobotType.DRONE){
+						System.out.print("D");
+						continue;
+					}
+				}
+				for(MapLocation tower: rc.senseEnemyTowerLocations()){
+					if(new MapLocation(i,j).equals(tower)){
+						System.out.print("T");
+						continue;
+					}
+				}
+				switch(rc.senseTerrainTile(new MapLocation(i,j))){
+				case VOID:
+					System.out.print("X");
+					break;
+				case OFF_MAP:
+					System.out.print("O");
+					break;
+				case NORMAL:
+					System.out.print(" ");
+					break;
+				case UNKNOWN:
+					System.out.print("U");
+					break;					
+				}
+			}
+			System.out.println();
 		}
 	}
 	private static void supplyChain() {
@@ -200,9 +287,15 @@ public class RobotPlayer {
 	
 	public static boolean safeToMove(Direction dir){
 		boolean safe = true;
-		RobotInfo[] enemiesInRange = rc.senseNearbyRobots(rc.getType().attackRadiusSquared, rc.getTeam().opponent());
+		RobotInfo[] enemiesInRange = rc.senseNearbyRobots(10000, rc.getTeam().opponent());
 		for(RobotInfo ri: enemiesInRange){
 			if(rc.getLocation().add(dir).distanceSquaredTo(ri.location)<=ri.type.attackRadiusSquared){
+				safe = false;
+				break;
+			}
+		}
+		for(MapLocation loc: rc.senseEnemyTowerLocations()){
+			if(rc.getLocation().add(dir).distanceSquaredTo(loc)<=RobotType.TOWER.attackRadiusSquared){
 				safe = false;
 				break;
 			}
@@ -216,7 +309,7 @@ public class RobotPlayer {
 		while(true){
 			MapLocation[] nearby = MapLocation.getAllMapLocationsWithinRadiusSq(rc.getLocation(), i*i);
 			for(MapLocation near: nearby){
-				if(rc.senseTerrainTile(near) == TerrainTile.UNKNOWN && rc.senseTerrainTile(near) != TerrainTile.OFF_MAP){
+				if(rc.senseTerrainTile(near) == TerrainTile.UNKNOWN && isLocationInMap(near)){
 					target = near;
 				}
 			}
@@ -230,10 +323,20 @@ public class RobotPlayer {
 	
 	private static void pathToLocation(MapLocation target, int threshold) throws GameActionException {
 		while(rc.getLocation().distanceSquaredTo(target)>threshold){
-			moveAsCloseToDirection(rc.getLocation().directionTo(target));
+			moveAsCloseToDirectionSafe(rc.getLocation().directionTo(target));
+			rc.setIndicatorString(0, ""+target);
 			rc.yield();
 		}
 		
+	}
+	
+	public static boolean isLocationInMap(MapLocation toTest){
+		if(toTest.x < centerOfMap.x + widthOfMap/2 && toTest.x > centerOfMap.x - widthOfMap/2){
+			if(toTest.y < centerOfMap.y + heightOfMap/2 && toTest.y > centerOfMap.y - heightOfMap/2){
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	
